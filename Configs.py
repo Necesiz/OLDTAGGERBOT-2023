575f1819b3373b6d6132882eced7c92f7672444e
Necesiz <97000998+Necesiz@users.noreply.github.com>
2022-12-11 02:32:24 +0400

    Delete Configs.py

--- Configs.py
+++
@@ -1,8 +0,0 @@
-# Zorunlu gereklidir. Eklemek istediğin bilgileri burda belirt çekinme 😏
-
-import os
-
-class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", None)) 
-    API_HASH = os.environ.get("API_HASH", None)
-    TOKEN = os.environ.get("TOKEN", None)
