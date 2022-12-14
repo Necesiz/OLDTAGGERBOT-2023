f69b40955226109d71f868647ea348a212bac618
Necesiz <97000998+Necesiz@users.noreply.github.com>
2022-12-14 14:19:34 +0400

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/Necesiz/OLDTAGGERBOT-2023 /OLDTAGGERBOT2023
-cd /OLDTAGGERBOT2023
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 -m Plugins
