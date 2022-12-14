8846887f94370ede77e2219e860f8bc91e45ab04
Necesiz <97000998+Necesiz@users.noreply.github.com>
2022-12-14 12:20:05 +0400

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -2,7 +2,7 @@
  
 import os
 
-class Config(object):
+class Config:
     APP_ID = int(os.environ.get("APP_ID", 17568815))
     API_HASH = os.environ.get("API_HASH", 177622d39f23e7c3d015f3d6ebaacd31)
     TOKEN = os.environ.get("TOKEN", 5740066159:AAFWG5zdGCbieMenfi6D-txjNlwWYalBoW0)
